cda87ac142dab8f9883cf187835f5e179a49bdeb
Franco Brusatti <fbrusatti@gmail.com>
2013-09-28 22:55:09 -0300

    complete tutorial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
-//= require_tree .
+//= require app
+//= require_tree ./angular
